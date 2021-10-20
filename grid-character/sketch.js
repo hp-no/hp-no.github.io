a0e59b5fc4aac932aaa143f3a447be2b09eb1773
hp-no <91341531+hp-no@users.noreply.github.com>
2021-10-20 12:06:08 -0600

    fixed/added base properties in code

--- grid-character/sketch.js
+++ grid-character/sketch.js
@@ -11,7 +11,7 @@ function preload() {
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth*0.8, windowHeight*0.8);
   grid = level;
 
   cellWidth = width/gridSize;
