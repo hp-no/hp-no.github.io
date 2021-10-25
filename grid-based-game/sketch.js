58abdbe40a3d75216548489928e3636ace258f5e
hp-no <91341531+hp-no@users.noreply.github.com>
2021-10-25 12:09:32 -0600

    added movement of platform

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -4,61 +4,78 @@ let grid, block;
 let gridDimensions = 30;
 let cellWidth, cellHeight;
 let layer = 0;
-let dx = 0;
+let dx = 1;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  // Note: MAKE CHANGES TO DO windowwidth *windowwidth OR windowHeight*windowHeight
+  createCanvas(windowWidth*0.9, windowHeight*0.9);
   grid = createEmpty2DArray(gridDimensions, gridDimensions);
   
   cellWidth = width/gridDimensions;
   cellHeight = height/gridDimensions;
 
   block = {
-    blockX: 0,
-    blockY: gridDimensions - layer,
+    x: 0,
+    y: gridDimensions-2 //- layer, temporarily 5
   };
+
+  //put block on grid
+  grid[block.y][block.x] = 9;
 }
 
 function draw() {
   background(220);
-  // displayGrid();
+  displayGrid();
 
-  // // if (frameCount % 10 === 0) {
-  moveBlocks(blockX+1, blockY);
-  // }
-
-}
-
-function mousePressed() {
-  let cellWidth = width/gridDimensions;
-  let cellHeight = height/gridDimensions;
-
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
-
-  if (grid[cellY][cellX] === 1) {
-    grid[cellY][cellX] = 0;
-  }
-  else if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
+  if (frameCount % 5 === 0) {
+    moveBlocks(block.x+dx);
   }
 }
+
+// function mousePressed() {
+//   let cellWidth = width/gridDimensions;
+//   let cellHeight = height/gridDimensions;
+
+//   let cellX = Math.floor(mouseX/cellWidth);
+//   let cellY = Math.floor(mouseY/cellHeight);
+
+//   if (grid[cellY][cellX] === 1) {
+//     grid[cellY][cellX] = 0;
+//   }
+//   else if (grid[cellY][cellX] === 0) {
+//     grid[cellY][cellX] = 1;
+//   }
+// }
 
 function moveBlocks(newX) {
-  grid[blockY][x-1] = 1;
-  grid[blockY][x] = 1;
-  grid[blockY][x+1] = 1;
-
+  if (newX-1 >= 0 && newX+1 < gridDimensions) {
+    //set previous location to empty
+    grid[block.y][block.x-1] = 0; //back end
+    grid[block.y][block.x] = 0; //middle block
+    grid[block.y][block.x+1] = 0; //front end
+    block.x = newX;
+  
+    //fill in new location spot
+    grid[block.y][block.x-1] = 9; //back end
+    grid[block.y][block.x] = 9; //middle block
+    grid[block.y][block.x+1] = 9; //front end
+  }
+  else {
+    dx = -dx;
+  }
 }
 
 function displayGrid() {
-  for (let y = 0; y<gridDimensions; y++) {
+  for (let y=0; y<gridDimensions; y++) {
     for (let x=0; x<gridDimensions; x++) {
       if (grid[y][x] === 0) {
         fill("white");
       }
       else if (grid[y][x] === 1) {
         fill("black");
+      }
+      else if (grid[y][x] === 9) {
+        fill("red");
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
