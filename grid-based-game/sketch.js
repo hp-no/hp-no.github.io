c2c2a8b0866516573415acb465e1d2eaaa00ee40
hp-no <91341531+hp-no@users.noreply.github.com>
2021-11-03 12:11:22 -0600

    add detection of lower platform

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -3,9 +3,16 @@
 // ***Input comment header here****
 
 // To Do: After finishing the basic function, work on the "deletion of blocks", add # of blocks present to OOP constructors
-// If part of platform does not overlap the stack, delete the non-overlapping blocks 
+// If part of platform does not overlap the stack, delete the non-overlapping blocks (keeps track of # of blocks still alive in platform)
 // (implement a function/code that checks to see # of blocks that overlap and creates a new platform according to that #)
+// Include a restart screen, where if you lose all your blocks or want to restart, with the click of a button on screen/keyboard,
+// game restarts/load its original state (clears all blocks, restarts layers, platform #1); USE A FUNCTION TO DO THIS
+// Fix and neaten up code + comments
 
+// If incorportating a start/title menu, incorporate removeElments(); command to get remove the text and move onto the game screen
+// what is touchStarted();?
+
+// Global Variables
 
 let platformArray = [];
 let startingPlatform;
@@ -17,6 +24,7 @@ let row = 0; //let layer = 0
 let dx = 1;
 
 function setup() {
+  // Establish a grid
   if (windowWidth < windowHeight) {
     createCanvas(windowWidth*0.9, windowWidth*0.9);
   }
@@ -28,8 +36,8 @@ function setup() {
   cellWidth = width/gridDimensions;
   cellHeight = height/gridDimensions;
 
+  // Create inital platforms
   startingPlatform = new Platform(width/2, -1); // base foundation/platform the player stacks on
-
   let thePlatform = new Platform(random(gridDimensions), row);
   platformArray.push(thePlatform);
 
@@ -61,6 +69,7 @@ class Platform {
     this.y = gridDimensions-3 - row;
     this.dx = 3;
     this.state = "move";
+    
   }
 
   move() {
@@ -85,10 +94,12 @@ class Platform {
   }
 
   place() {
-    this.state = "place"; // this stops the prior movement function
-    grid[this.y][this.x-1] = 1;
-    grid[this.y][this.x] = 1;
-    grid[this.y][this.x+1] = 1;
+    this.state = "place"; // stops platform movement
+    for (let i = -1; i <= 1; i++) {
+      if (grid[this.y - 1][this.x + i] === 1) { // checks if there is a block on row below that can be stacked on; place block down if true
+        grid[this.y][this.x + i] = 1;
+      }
+    }
   }
 }
 
@@ -107,6 +118,7 @@ function keyPressed() {
 function displayGrid() {
   for (let y=0; y<gridDimensions; y++) {
     for (let x=0; x<gridDimensions; x++) {
+      noStroke();
       if (grid[y][x] === 0) {
         fill("white");
       }
@@ -114,6 +126,7 @@ function displayGrid() {
         fill("black");
       }
       else if (grid[y][x] === 9) {
+        stroke();
         fill("red");
       }
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
@@ -131,89 +144,3 @@ function createEmpty2DArray(rows, cols) {
   }
   return grid;
 }
-
-
-// Draft for using OOP in grid-based game:
-
-// class Platform {
-//   constructor(x, row) {
-//     this.x = x;
-//     this.y = gridDimensions-3 - row;
-//     this.dx = 3;
-//   }
-
-//   moveOrPlace() {
-//     if (state === "move") {
-//       this.move();
-//     }
-//     else if (state === "place") {
-//       this.place();
-//     }
-//   }
-
-//   move() {
-//     if (this.x-1 >= 0 && this.x+1 <= gridDimensions) {
-//       //set previous location to empty
-//       grid[this.y][this.x-1] = 0;
-//       grid[this.y][this.x] = 0;
-//       grid[this.y][this.x+1] = 0;
-  
-//       this.x += this.dx;
-  
-//       //fill in new location spot
-//       grid[this.y][this.x-1] = 9;
-//       grid[this.y][this.x] = 9;
-//       grid[this.y][this.x+1] = 9;
-//     }
-//     else {
-//       this.dx = -this.dx;
-//     }
-//   }
-
-//   place() {
-//     grid[this.y][this.x-1] = 1;
-//     grid[this.y][this.x] = 1;
-//     grid[this.y][this.x+1] = 1;
-
-//     this.y -= 1;
-
-//     state = "move";
-//   }
-// }
-
-
-// OLD CODE:
-
-// function moveBlocks(newX, newY) {
-//   if (state === "idle") {
-//     if (newX-1 >= 0 && newX+1 < gridDimensions) {
-//       //set previous location to empty
-//       grid[block.y][block.x-1] = 0; //back block
-//       grid[block.y][block.x] = 0; //middle block
-//       grid[block.y][block.x+1] = 0; //front block
-  
-//       block.x = newX;
-//       block.y = newY;
-  
-//       //fill in new location spot
-//       grid[block.y][block.x-1] = 9; //back block
-//       grid[block.y][block.x] = 9; //middle block
-//       grid[block.y][block.x+1] = 9; //front block
-//     }
-//     else {
-//       dx = -dx;
-//     }
-//   }
-//   else if (state === "new layer") {
-//     placeBlocks(newX, newY);
-
-//     state = "idle";
-//     layer++;
-//   }
-// }
-
-// function placeBlocks(x, y) {
-//   grid[y][x-1] = 1; //back block
-//   grid[y][x] = 1; //middle block
-//   grid[y][x+1] = 1; //front block
-// }
