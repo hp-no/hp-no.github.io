5f371a3fcc32d7d048ec5f06135a1ea37f1c727a
hp-no <91341531+hp-no@users.noreply.github.com>
2021-11-04 12:11:11 -0600

    development of adding/subtracting platform length

--- grid-based-game/sketch.js
+++ grid-based-game/sketch.js
@@ -2,26 +2,33 @@
 
 // ***Input comment header here****
 
-// To Do: After finishing the basic function, work on the "deletion of blocks", add # of blocks present to OOP constructors
+// To Do: After finishing the basic function, work on the "deletion of blocks", 
+// ***Incorporate the # of blocks present to OOP constructors***
 // If part of platform does not overlap the stack, delete the non-overlapping blocks (keeps track of # of blocks still alive in platform)
 // (implement a function/code that checks to see # of blocks that overlap and creates a new platform according to that #)
 // Include a restart screen, where if you lose all your blocks or want to restart, with the click of a button on screen/keyboard,
 // game restarts/load its original state (clears all blocks, restarts layers, platform #1); USE A FUNCTION TO DO THIS
 // Fix and neaten up code + comments
 
+//Incorporate a "if blocks !== 0" thing
+
 // If incorportating a start/title menu, incorporate removeElments(); command to get remove the text and move onto the game screen
 // what is touchStarted();?
 
 // Global Variables
 
-let platformArray = [];
-let startingPlatform;
-let grid, block;
-let state = "idle";
+let grid; //block
 let gridDimensions = 30;
 let cellWidth, cellHeight;
+
+let platformArray = [];
+let startingPlatform;
+let removedBlocks = 0;
 let row = 0; //let layer = 0
 let dx = 1;
+let state = "idle";
+
+
 
 function setup() {
   // Establish a grid
@@ -37,15 +44,10 @@ function setup() {
   cellHeight = height/gridDimensions;
 
   // Create inital platforms
-  startingPlatform = new Platform(width/2, -1); // base foundation/platform the player stacks on
-  let thePlatform = new Platform(random(gridDimensions), row);
+  startingPlatform = new Platform(width/2, -1, removedBlocks); // base foundation/platform the player stacks on
+  let thePlatform = new Platform(random(gridDimensions), row, removedBlocks);
   platformArray.push(thePlatform);
 
-  // block = {
-  //   x: 0,
-  //   y: gridDimensions-2,
-  //   speed: 5, //the lower the number, the faster
-  // };
 }
 
 function draw() {
@@ -64,28 +66,30 @@ function draw() {
 }
 
 class Platform {
-  constructor(x, row) {
+  constructor(x, row, removedBlocks) {
     this.x = x;
     this.y = gridDimensions-3 - row;
     this.dx = 3;
     this.state = "move";
-    
+    this.platformLength = 3 - removedBlocks;
   }
 
   move() {
     if (this.state === "move") {
       if (this.x-1 >= 0 && this.x+1 <= gridDimensions) {
-        //set previous location to empty
-        grid[this.y][this.x-1] = 0;
-        grid[this.y][this.x] = 0;
-        grid[this.y][this.x+1] = 0;
+        // //set previous location to empty
+        // grid[this.y][this.x-1] = 0;
+        // grid[this.y][this.x] = 0;
+        // grid[this.y][this.x+1] = 0;
     
-        this.x += this.dx;
+        // this.x += this.dx;
     
-        //fill in new location spot
-        grid[this.y][this.x-1] = 9;
-        grid[this.y][this.x] = 9;
-        grid[this.y][this.x+1] = 9;
+        // //fill in new location spot
+        // grid[this.y][this.x-1] = 9;
+        // grid[this.y][this.x] = 9;
+        // grid[this.y][this.x+1] = 9;
+
+        for (let i = -1; i < this.platformLength - 1; i++)
       }
       else {
         this.dx = -this.dx;
@@ -95,9 +99,14 @@ class Platform {
 
   place() {
     this.state = "place"; // stops platform movement
+
     for (let i = -1; i <= 1; i++) {
-      if (grid[this.y - 1][this.x + i] === 1) { // checks if there is a block on row below that can be stacked on; place block down if true
-        grid[this.y][this.x + i] = 1;
+    // checks if there is a block on row below
+      if (grid[this.y - 1][this.x + i] === 1) { 
+        grid[this.y][this.x + i] = 1; // if true, places block down
+      }
+      else {
+        numberOfBlocks -= 1; // if false, removes the block
       }
     }
   }
@@ -110,7 +119,7 @@ function keyPressed() {
 
     row++;
 
-    let thePlatform = new Platform(random(gridDimensions), row);
+    let thePlatform = new Platform(random(gridDimensions), row, numberOfBlocks);
     platformArray.push(thePlatform);
   }
 }
