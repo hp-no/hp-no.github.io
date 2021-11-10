23183b1bc64acca9273b4de87481da0ec4513a26
hp-no <91341531+hp-no@users.noreply.github.com>
2021-11-09 23:28:02 -0700

    updated movement of characters

--- grid-practice/sketch.js
+++ grid-practice/sketch.js
@@ -28,95 +28,93 @@ function setup() {
 function draw() {
   background(220);
   displayGrid();
+  if (frameCount % 30 === 0) {
+    theCat.randomMovement();
+  }
   theCat.display();
   player.display();
-  // movePlayer();
+
 }
 
 class Character {
   constructor(x, y, theColor) {
     this.x = x;
     this.y = y;
-    this.dx = cellWidth; //cellWidth*0.07;
-    this.dy = cellHeight; //cellHeight*0.07;
     this.radius = (cellWidth+cellHeight)/2*0.325;
     this.theColor = theColor;
+
   }
-   
+
   display() {
     fill(this.theColor);
     circle(this.x*cellWidth + cellWidth/2, this.y*cellHeight + cellHeight/2, this.radius*2);
   }
 
   randomMovement() {
-    grid[this.y][this.x] = 9;
+    let choice = random(100);
+    if (choice < 25) { // down
+      while (grid[this.x][this.y-1] === 0) {
+        this.moveTo(this.x, this.y-1);
+      }
+    }
+    else if (choice < 50) { // up
+      this.moveTo(this.x, this.y+1);
+    }
+    else if (choice < 75) { // left
+      this.moveTo(this.x-1, this.y);
+    }
+    else { // right
+      this.moveTo(this.x+1, this.y);
+    }
+  }
+
+  moveTo(newX, newY) {
+    //make sure there's no walls before moving
+    if (grid[newY][newX] === 0) {
+      this.x = newX;
+      this.y = newY;
+    }
   }
 }
-
 
 function keyPressed() {
   if (key === "w") {
-    if (grid[player.y - player.dy][player.x] === 0) {
-     player.y -= player.dy;
-   }
-
+    player.moveTo(player.x, player.y-1);
   }
   if (key === "a") {
-    newLocation(player.x - player.dx, player.y);
+    player.moveTo(player.x-1, player.y);
   }
   if (key === "s") {
-    newLocation(player.x, player.y + player.dy);
+    player.moveTo(player.x, player.y+1);
   }
   if (key === "d") {
-    newLocation(player.x + player.dx, player.y);
+    player.moveTo(player.x+1, player.y);
   }
 }
 
-function newLocation(newX, newY) {
-  return grid[newY][newX] === 0;
-}
+// function mousePressed() {
+//   let cellWidth = width/gridSize;
+//   let cellHeight = height/gridSize;
 
-// function movePlayer() {
-//   if (keyIsDown(87)) {
-//     player.y -= player.dy;
+//   let cellX = Math.floor(mouseX/cellWidth);
+//   let cellY = Math.floor(mouseY/cellHeight);
+
+//   if (grid[cellY][cellX] === 1) {
+//     grid[cellY][cellX] = 0;
 //   }
-//   if (keyIsDown(65)) {
-//     player.x -= player.dx;
-//   }
-//   if (keyIsDown (83)) {
-//     player.y += player.dy;
-//   }
-//   if (keyIsDown(68) && grid[player.y][player.x+1] === 0) {
-//     player.x += player.dx;
+//   else if (grid[cellY][cellX] === 0) {
+//     grid[cellY][cellX] = 1;
 //   }
 // }
-
-function mousePressed() {
-  let cellWidth = width/gridSize;
-  let cellHeight = height/gridSize;
-
-  let cellX = Math.floor(mouseX/cellWidth);
-  let cellY = Math.floor(mouseY/cellHeight);
-
-  if (grid[cellY][cellX] === 1) {
-    grid[cellY][cellX] = 0;
-  }
-  else if (grid[cellY][cellX] === 0) {
-    grid[cellY][cellX] = 1;
-  }
-}
 
 function displayGrid() {
   for (let y=0; y<gridDimensions; y++) {
     for (let x=0; x<gridDimensions; x++) {
-      if (grid[y][x] === 0) {
+      if (grid[y][x] !== 1) {
         fill(0);
       }
-      else if (grid[y][x] === 1) {
+      if (grid[y][x] === 1) {
         fill("blue");
-      }
-      else if (grid[y][x] === 9) {
-        fill("red");
       }
       noStroke();
       rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
