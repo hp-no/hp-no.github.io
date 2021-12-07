9e9d2821da3bb228afdf53c6ef0c11c0f52253cc
hp-no <91341531+hp-no@users.noreply.github.com>
2021-12-07 12:10:56 -0600

    implementation of a 3D array

--- 3d-testing/sketch.js
+++ 3d-testing/sketch.js
@@ -1,17 +1,22 @@
+let p5 = require("p5");
 
 // Experimenting with 3D features and models
 let smallBoat;
+let bottle, bottleCoords;
 
 function preload() {
   smallBoat = loadModel("assets/boat_small.obj", true);
+  bottle = loadModel("assets/bottle.obj", true);
 }
 
 function setup() {
-  createCanvas(windowWidth, windowHeight, WEBGL);
+  createCanvas(windowWidth*0.95, windowHeight*0.95, WEBGL);
+  bottleCoords = new p5.Geometry();
 }
 
 function draw() {
   background(220);
-  rotateY(frameCount * 0.01);
-  model(smallBoat);
+  rotateX(135);
+  // rotateY(frameCount * 0.01);
+  model(bottle);
 }
